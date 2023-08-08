9a449aa0f87c1c85287957220898352bbb8e9d15
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-08 09:38:20 +0000

    5-4終了

--- js/script.js
+++ js/script.js
@@ -8,8 +8,15 @@ const lists = [
   'fig.jpg',
   'apple.jpg',
 ];
-console.log(lists);
+//console.log(lists[0]);
 
-const content = `<div><img src="images/strawberry.jpg" alt=""></div>`;
+const content = `<div><img src="images/${lists[0]}" alt=""></div>
+<div><img src="images/${lists[1]}" alt=""></div>
+<div><img src="images/${lists[2]}" alt=""></div>
+<div><img src="images/${lists[3]}" alt=""></div>
+<div><img src="images/${lists[4]}" alt=""></div>
+<div><img src="images/${lists[5]}" alt=""></div>
+`;
+
 //menu.textContent = content;
 menu.insertAdjacentHTML('beforeend', content);
